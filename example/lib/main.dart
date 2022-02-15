b1082c13a7627191fae3c6ab10eceb8051f053c4
svetlana.ryzhkova <svetlana.ryzhkova@akvelon.com>
2022-02-15 14:52:12 +0300

    redistributed files

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,112 +1,26 @@
-import 'package:example/custom_code_box.dart';
-// import 'package:example/readme/readme_examples.dart';
+//File only for testing. Later file will be deleted.
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:code_text_field/constants/constants.dart';
+import 'custom_code_box.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const CodeEditor());
 }
 
-class MyApp extends StatelessWidget {
+class CodeEditor extends StatelessWidget {
+  const CodeEditor({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Code field',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  Future _launchInBrowser(String url) async {
-    if (await canLaunch(url)) {
-      await launch(
-        url,
-        forceSafariVC: false,
-        forceWebView: false,
-        headers: <String, String>{'my_header_key': 'my_header_value'},
-      );
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final preset = <String>[
-      "go|monokai-sublime",
-    ];
-    List<Widget> children = preset.map((e) {
-      final parts = e.split('|');
-      print(parts);
-      final box = CustomCodeBox(
-        language: parts[0],
-        theme: parts[1],
-      );
-      return Padding(
-        padding: EdgeInsets.only(bottom: 32.0),
-        child: box,
-      );
-    }).toList();
-    final page = Center(
-      child: Container(
-        constraints: BoxConstraints(maxWidth: 900),
-        padding: EdgeInsets.symmetric(vertical: 32.0),
-        child: Column(children: children),
-      ),
-    );
-
-    // return Scaffold(
-    //   appBar: AppBar(),
-    //   body: CodeEditor(),
-    // );
-
-    return Scaffold(
-      backgroundColor: Color(0xFF363636),
-      appBar: AppBar(
-        backgroundColor: Color(0xff23241f),
-        title: Text("CodeField demo"),
-        // title: Text("Recursive Fibonacci"),
-        centerTitle: false,
-        actions: [
-          TextButton.icon(
-            style: TextButton.styleFrom(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              primary: Colors.white,
-            ),
-            icon: Icon(FontAwesomeIcons.github),
-            onPressed: () =>
-                _launchInBrowser("https://github.com/BertrandBev/code_field"),
-            label: Text("GITHUB"),
+      home: Scaffold(
+        body: CustomCodeBox(
+            language: dart,
+            theme: 'monokai-sublime',
           ),
-          SizedBox(width: 8.0),
-        ],
-      ),
-      body: SingleChildScrollView(child: page),
-      // body: CodeEditor5(),
+      ) 
     );
   }
 }
